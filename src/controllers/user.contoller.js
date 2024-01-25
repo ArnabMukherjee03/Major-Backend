eb45b4e9fd1073067170056fe11c7c7ebabc2279
Arnab Mukherjee <arnabmukherjee58260@gmail.com>
2024-01-25 14:37:35 +0530

    Register user route done

---
+++ src/controllers/user.contoller.js
@@ -0,0 +1,60 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandeler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username,fullname, password } = await req.body;
+
+  if (
+    [email, username, fullname, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fileds are required");
+  }
+
+  const regex_pwd =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()-=_+{}[\]:;<>,.?/~]).{8,24}$/;
+
+  if (!regex_pwd.test(password)) {
+    throw new ApiError(400, "Please Use a strong Password");
+  }
+
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+//   console.log(avatarLocalPath);
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const user = await User.create({
+    fullname,
+    avatar: avatarLocalPath,
+    email,
+    password,
+    username,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
+});
+
+
+
+export {registerUser};
---
+++ src/routes/user.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {upload} from "../middleware/multer.middleware.js"
+import { registerUser } from "../controllers/user.contoller.js";
+
+const router = Router()
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
+
+
+export default router;
